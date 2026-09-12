26b4585e049d65ec986dd13cce04d3ff37d4bdca
qhuyy0411 <huyngo042007@gmail.com>
2026-09-12 11:01:23 +0700

    Delete youtube.request.js

--- youtube.request.js
+++
@@ -1,9 +0,0 @@
-/***********************************************
- * YouTube Request Filter for Shadowrocket
- ***********************************************/
-let url = $request.url;
-if (url.includes("googlevideo.com")) {
-    // Xử lý loại bỏ tham số quảng cáo trong request video
-    url = url.replace(/&ctier=L(&.+?),ctier,(.+)/, "$1$2");
-}
-$done({ url: url });
